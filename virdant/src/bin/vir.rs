583dcc4299a5d5c67d25a3eeb53e0b0045fcaa2c
Vir Codificat <vircodificat@gmail.com>
2026-04-06 16:22:43 -0700

    Fix vir new boilerplate.

--- virdant/src/bin/vir.rs
+++ virdant/src/bin/vir.rs
@@ -489,7 +489,7 @@ fn new_project(args: &Args, project: &str) {
     );
     std::fs::write(project_dir.join("Virdant.toml"), toml_content).unwrap();
 
-    let top_vir_content = "export mod Top {\n    incoming clock : Clock;\n}\n";
+    let top_vir_content = include_str!("../../../examples/blink.vir");
     std::fs::write(project_dir.join("src").join("top.vir"), top_vir_content).unwrap();
 
     // Walk up from cwd looking for an existing .git directory
